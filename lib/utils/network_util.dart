a5ca7c629fd571853760816720d839ffccba8db2
Chirag Saraogi <gunjansaraogi@Gunjan.local>
2019-07-13 23:55:07 +0530

    first commit

---
+++ lib/utils/network_util.dart
@@ -0,0 +1,10 @@
+class NetworkUtil{
+  static NetworkUtil _instance = NetworkUtil.internal();
+  NetworkUtil.internal();
+  factory NetworkUtil() => _instance;
+
+  Future<dynamic> get(){
+    return null;
+  }
+
+}
